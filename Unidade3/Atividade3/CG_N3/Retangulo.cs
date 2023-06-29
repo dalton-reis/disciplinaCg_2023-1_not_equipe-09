a526130480280e919172fbe7495c72d3732b06f4
EduardoPhilippeCosta <epcdudu@gmail.com>
2023-06-28 23:07:56 -0300

    Revert "."
    This reverts commit 74b6840da7ba2482563a3821ee51969b9fafe61f.

--- Unidade3/Atividade3/CG_N3/Retangulo.cs
+++ Unidade3/Atividade3/CG_N3/Retangulo.cs
@@ -5,46 +5,39 @@ using OpenTK.Graphics.OpenGL4;
 
 namespace gcgcg
 {
-    internal class Retangulo : Objeto
+  internal class Retangulo : Objeto
+  {
+    public Retangulo(Objeto paiRef, ref char _rotulo) : 
+      this(paiRef, ref _rotulo, new Ponto4D(-0.5,-0.5), new Ponto4D(0.5,0.5)) {}
+
+    public Retangulo(Objeto paiRef, ref char _rotulo, Ponto4D ptoInfEsq, Ponto4D ptoSupDir) : base(paiRef, ref _rotulo)
     {
-        public Retangulo(Objeto paiRef, ref char _rotulo) :
-          this(paiRef, ref _rotulo, new Ponto4D(-0.5, -0.5), new Ponto4D(0.5, 0.5))
-        { }
+      PrimitivaTipo = PrimitiveType.Points;
+      PrimitivaTamanho = 10;
 
-        public Retangulo(Objeto paiRef, ref char _rotulo, Ponto4D ptoInfEsq, Ponto4D ptoSupDir) : base(paiRef, ref _rotulo)
-        {
-            PrimitivaTipo = PrimitiveType.Points;
-            PrimitivaTamanho = 10;
+      // Sentido horário
+      base.PontosAdicionar(ptoInfEsq);
+      base.PontosAdicionar(new Ponto4D(ptoSupDir.X, ptoInfEsq.Y));
+      base.PontosAdicionar(ptoSupDir);
+      base.PontosAdicionar(new Ponto4D(ptoInfEsq.X, ptoSupDir.Y));
+      Atualizar();
+    }
 
-            // Sentido horário
-            base.PontosAdicionar(ptoInfEsq);
-            base.PontosAdicionar(new Ponto4D(ptoSupDir.X, ptoInfEsq.Y));
-            base.PontosAdicionar(ptoSupDir);
-            base.PontosAdicionar(new Ponto4D(ptoInfEsq.X, ptoSupDir.Y));
-            Atualizar();
-        }
-        private float[] _textureCoordinates = {
-    0.0f, 0.0f,
-    1.0f, 0.0f,
-    1.0f, 1.0f,
-    0.0f, 1.0f
-};
+    private void Atualizar()
+    {
 
-        private void Atualizar()
-        {
-
-            base.ObjetoAtualizar();
-        }
+      base.ObjetoAtualizar();
+    }
 
 #if CG_Debug
-        public override string ToString()
-        {
-            string retorno;
-            retorno = "__ Objeto Retangulo _ Tipo: " + PrimitivaTipo + " _ Tamanho: " + PrimitivaTamanho + "\n";
-            retorno += base.ImprimeToString();
-            return (retorno);
-        }
+    public override string ToString()
+    {
+      string retorno;
+      retorno = "__ Objeto Retangulo _ Tipo: " + PrimitivaTipo + " _ Tamanho: " + PrimitivaTamanho + "\n";
+      retorno += base.ImprimeToString();
+      return (retorno);
+    }
 #endif
 
-    }
+  }
 }
